adbdd55af5af45a9049eca957cc2c6b03cbca22c
Charlotte Wellington <charlottenaaodarleywellington@gmail.com>
2022-04-08 22:06:12 +0000

    I added new codes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,22 @@
+import "./intro.css"
 const Intro = ( ) => {
     return(
-        <div>
-            this is intro
+        <div className= "i">
+           <div className="i-left"> 
+               <div className="i-left-wrapper">
+                   <h2 className="i-intro">Hello,My name is</h2>
+                   <h1 className="i-name">Charlotte Wellington</h1>
+                   <div className="i-title">
+                       <div className="i-title-wrapper">
+                           <div className="i-title-item">Web Developer</div>
+                           <div className="i-title-item">Content Creator</div>
+                           <div className="i-title-item">Software Engineer</div>
+                           <div className="i-title-item">Student</div>
+                           </div>
+                   </div>
+               </div> 
+           </div>
+           <div className="i-right">right</div>
         </div>
     )
 }
