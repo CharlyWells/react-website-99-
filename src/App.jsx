8e001fd52fbdffaa2562e58c04e913a820fb3df0
Charlotte Wellington <charlottenaaodarleywellington@gmail.com>
2022-04-10 04:45:09 +0000

    I made changes to the codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import About  from "./components/about/About"; 
 import Intro from "./components/intro/Intro";
 const App = () => {
-    return<div><Intro/></div>
-    return<div><About/></div>  
-};
-export default App;
+    return<div>
+        <Intro />
+        <About />
+        </div>
+}
+     
+export default App; 
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,28 @@
 import "./About.css"
+import Award from "../../img/Award.jpg"
 const About = ( ) => {
     return(
         <div className= "aboutMe" >
-                About      
-        </div>
-    )
-}
+             
+            <div className="aboutMe-left">
+                <div className="aboutMe-card bg"></div>
+                <div className="aboutMe-card">
+                    <img src=" https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?cs=srgb&dl=pexels-pixabay-39284.jpg&fm=jpg " alt=""  className="aboutMe-img" />
+                </div>
+            </div>
+            <div className="aboutMe-right">
+            <h1 className="aboutMe-title">About Me</h1>
+         
+        <p className="aboutMe-desc">
+          A front-end web developer primarily doing  React. I am  also experienced in Java .
+        </p>
+         
+         </div>
+            </div>
+            
+           
+           
+    ) 
+} 
 
 export default About
