c960c22ef849a85e46297a668c31d59c40856823
Charlotte Wellington <charlottenaaodarleywellington@gmail.com>
2022-04-14 19:11:44 +0000

    I added new codes

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -6,22 +6,22 @@ const work= ()=>  {
         <div className="Work">
         <div className="Work-texts">
         <div className="Work-card bg"></div>
-            <h1 className="Work-title">What am I good at?</h1>
-           <p className="Work-desc">
-           I have listed my abilities are below
-           </p>
+            <h1 className="Work-title">My Work</h1>
+            
         </div>
         <div className="Work-left">
                 
         <div className="Work-card">
-                    <img src=" https://images.unsplash.com/photo-1621839673705-6617adf9e890?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80 " alt="Picture Of A Web development Work Area"  className="aboutMe-img" />
-                </div>
+         
+           <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6879807546769641473" height="500" width="504" frameborder="0" allowfullscreen="" title="Embedded post" ></iframe>
+                    </div>
              
             <div className="Work-right">
-            <h1 className="Work-title-t">Front-End Web Development</h1>
+            <h1 className="Work-title-t">Front-End</h1>
          
         <p className="Work-description">
-        I use React for Frontend-Web development.   
+           As part of the projects i decided to work on, I considered building a beauty parlor website to provide beauty services for clients. The main motive behind it was to have a sample website down for customers who intended to reach out to me to help them build up projects to have confidence in my ability to develop a front-end website
+        
         </p>
         </div>
          </div>
