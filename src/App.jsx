502d31f60c6d48f63b2a99436c2db2e957f4a6b4
Charlotte Wellington <charlottenaaodarleywellington@gmail.com>
2022-04-14 16:26:00 +0000

    I added work.css and work.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import About  from "./components/about/About"; 
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
+import Work from "./components/work/Work";
 const App = () => {
     return<div>
         <Intro />
         <About />
         <ProductList/>
+        <Work/>
+         
+         
         </div>
 }
      
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,26 +1,26 @@
-import "./ProductList.css"  
+import "./Work.css"  
 
 
-const ProductList= ()=>  {
+const work= ()=>  {
     return (
-        <div className="ProductList">
-        <div className="ProductList-texts">
-        <div className="ProductList-card bg"></div>
-            <h1 className="ProductList-title">What am I good at?</h1>
-           <p className="ProductList-desc">
+        <div className="Work">
+        <div className="Work-texts">
+        <div className="Work-card bg"></div>
+            <h1 className="Work-title">What am I good at?</h1>
+           <p className="Work-desc">
            I have listed my abilities are below
            </p>
         </div>
-        <div className="ProductList-left">
+        <div className="Work-left">
                 
-        <div className="ProductList-card">
+        <div className="Work-card">
                     <img src=" https://images.unsplash.com/photo-1621839673705-6617adf9e890?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80 " alt="Picture Of A Web development Work Area"  className="aboutMe-img" />
                 </div>
              
-            <div className="ProductList-right">
-            <h1 className="ProductList-title-t">Front-End Web Development</h1>
+            <div className="Work-right">
+            <h1 className="Work-title-t">Front-End Web Development</h1>
          
-        <p className="ProductList-description">
+        <p className="Work-description">
         I use React for Frontend-Web development.   
         </p>
         </div>
@@ -37,4 +37,4 @@ const ProductList= ()=>  {
 };
  
 
-export default ProductList; 
+export default work; 
